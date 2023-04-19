a731aeb31e2feacc48d37881e8d49f519c59ecd8
Mohit Punia <mohitpunia@innotechteam.in>
2023-04-19 11:25:01 +0530

    routing..

--- src/components/HomePage/homePage.js
+++ src/components/HomePage/homePage.js
@@ -5,67 +5,33 @@ import ContactUs from "../ContactUs/contactUs";
 import Message from "../Message/message";
 import Messages from "../Messages/messages";
 import messages from "../../message.json";
-import { useState, useEffect } from "react";
 import "./homePage.css";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  const [activeAbout, setActiveAbout] = useState(false);
-  const [activeContact, setActiveContact] = useState(false);
-  const [activeMessages, setActiveMessages] = useState(false);
-
-  const aboutColorChange = () => {
-    setActiveAbout(true);
-    setActiveMessages(false);
-    setActiveContact(false);
-  };
-  const contactColorChange = () => {
-    setActiveContact(true);
-    setActiveMessages(false);
-    setActiveAbout(false);
-  };
-  const messagesColorChange = () => {
-    setActiveMessages(true);
-    setActiveContact(false);
-    setActiveAbout(false);
-  };
-
   return (
     <>
       <div className="navbar-link-list">
-        <h3>
-          {" "}
-          <Link
-            to="/about"
-            id="about"
-            className={activeAbout ? "color" : "real-color"}
-            onClick={aboutColorChange}
-          >
-            About Us
-          </Link>
-        </h3>
-        <h3>
-          {" "}
-          <Link
-            to="/contact"
-            id="contact"
-            className={activeContact ? "color" : "real-color"}
-            onClick={contactColorChange}
-          >
-            Contact
-          </Link>
-        </h3>
-        <h3>
-          {" "}
-          <Link
-            to="/messages"
-            id="messages"
-            className={activeMessages ? "color" : "real-color"}
-            onClick={messagesColorChange}
-          >
-            Messages
-          </Link>
-        </h3>
+        <div>
+          <h3>
+            {" "}
+            <Link to="/about" id="about">
+              About Us
+            </Link>
+          </h3>
+          <h3>
+            {" "}
+            <Link to="/contact" id="contact">
+              Contact
+            </Link>
+          </h3>
+          <h3>
+            {" "}
+            <Link to="/messages" id="messages">
+              Messages
+            </Link>
+          </h3>
+        </div>
       </div>
       <Routes>
         <Route path="/about" element={<AboutUs />} />
